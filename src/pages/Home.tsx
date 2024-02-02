6784e1b55bf84f448f40941d9513efe023b03823
Danilo Bassi <dbassi@irobot.com>
2024-02-02 11:58:04 -0300

    Add tabs and move entire Home component logic to a custom hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,66 +1,77 @@
 import { PodiumChart } from '../components/charts/Podium';
 import { Loading } from '../components/utils/Loading';
 import { useGetBestTimes } from '../hooks/useFecha';
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { GlobalContext } from '../context/globalContext';
 import { matchInfoWithPlayers } from '../utils/utils';
 import { Link, useSearchParams } from 'react-router-dom';
 import { Tabs } from '../components/tabs/Tabs';
 import { Tooltip } from 'react-tooltip';
 
-export function Home() {
-  const { data, isLoading, error, refresh } = useGetBestTimes();
+function useHome() {
+  const {
+    data: bestTimes,
+    isLoading: isBestTimesLoading,
+    error: errorGettingBestTimes,
+    refresh: refreshBestTimes,
+  } = useGetBestTimes();
   const { isGlobalContextLoading, globalData, playersData } = useContext(GlobalContext);
   const [searchParams] = useSearchParams();
 
   // call refresh when globalData is ready
   useEffect(() => {
-    refresh();
+    refreshBestTimes();
   }, []);
 
+  const dataWithPlayers = bestTimes && playersData ? matchInfoWithPlayers(bestTimes, playersData) : [];
+  const searchParamFecha = parseInt(searchParams.get('fecha') || '1');
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(isGlobalContextLoading || isBestTimesLoading);
+  }, [isGlobalContextLoading, isBestTimesLoading]);
+
+  return {
+    isLoading,
+    searchParamFecha,
+    errorGettingBestTimes,
+    refreshBestTimes,
+    currentGlobalFecha: globalData?.FechaActual,
+    maxFechas: globalData?.MaxFechas,
+    dataWithPlayers,
+  };
+}
+
+export function Home() {
+  const {
+    dataWithPlayers,
+    maxFechas,
+    isLoading,
+    searchParamFecha,
+    errorGettingBestTimes,
+    refreshBestTimes,
+    currentGlobalFecha,
+  } = useHome();
+
   const renderContent = () => {
-    if (isLoading || isGlobalContextLoading) {
+    if (isLoading) {
       return <Loading style={{ height: '350px', marginTop: '25vh' }} />;
     }
 
-    const dataWithPlayers = matchInfoWithPlayers(data, playersData);
-    const currentGlobalFecha = globalData['FechaActual'];
-    const maxFechas = parseInt(globalData['MaxFechas']);
-    const searchParamFecha = parseInt(searchParams.get('fecha') || '1');
-
-    if (error) {
+    if (errorGettingBestTimes) {
       return (
-        <HomeError errorMessage={error}>
-          <button onClick={refresh}>Reintentar</button>
+        <HomeError errorMessage="Error obteniendo los mejores tiempos.">
+          <button onClick={refreshBestTimes}>Reintentar</button>
         </HomeError>
       );
     }
 
-    if (isNaN(searchParamFecha)) {
-      return (
-        <HomeError errorMessage="La fecha introducida no es valida.">
-          <Link to={`/home?fecha=${currentGlobalFecha}`}>
-            <button>Ir a la Fecha actual</button>
-          </Link>
-        </HomeError>
-      );
-    }
-
-    if (searchParamFecha > currentGlobalFecha) {
-      return (
-        <HomeError errorMessage="La fecha ingresada no está activa todavía.">
-          <Link to={`/home?fecha=${currentGlobalFecha}`}>
-            <button>Ir a la Fecha actual</button>
-          </Link>
-        </HomeError>
-      );
-    }
-
-    return (
+    const TABS = (
       <>
         <Tabs>
           {Array(maxFechas)
-            .fill('')
+            .fill(undefined)
             .map((_, i) => {
               const idx = i + 1;
               const disabled = idx > currentGlobalFecha;
@@ -74,20 +85,51 @@ export function Home() {
                 : {};
 
               return (
-                <Link to={`/home?fecha=${idx}`} key={idx}>
-                  <button
-                    disabled={disabled}
-                    {...disabledTooltipProps}
-                    className={idx === searchParamFecha ? 'focused' : ''}
-                  >
-                    Fecha {idx}
-                  </button>
+                <Link
+                  to={`/home?fecha=${idx}`}
+                  key={idx}
+                  className={`btn ${idx === searchParamFecha ? 'focused' : ''} ${disabled ? 'disabled' : ''}`}
+                  {...disabledTooltipProps}
+                  onClick={(event) => (disabled ? event.preventDefault() : null)}
+                >
+                  Fecha {idx}
                 </Link>
               );
             })}
         </Tabs>
-        <PodiumChart data={dataWithPlayers} dataKey={`Fecha${searchParamFecha}`} />
         <Tooltip id="home-disabled-tooltip" style={{ backgroundColor: 'var(--button-hover)' }} />
+      </>
+    );
+
+    if (searchParamFecha > currentGlobalFecha) {
+      return (
+        <>
+          {TABS}
+          <HomeError errorMessage={`La Fecha ${searchParamFecha} no está activa todavía.`}>
+            <Link to={`/home?fecha=${currentGlobalFecha}`} className="btn">
+              Ir a la Fecha actual
+            </Link>
+          </HomeError>
+        </>
+      );
+    }
+    if (isNaN(searchParamFecha)) {
+      return (
+        <>
+          {TABS}
+          <HomeError errorMessage="La fecha introducida no es valida.">
+            <Link to={`/home?fecha=${currentGlobalFecha}`} className="btn">
+              Ir a la Fecha actual
+            </Link>
+          </HomeError>
+        </>
+      );
+    }
+
+    return (
+      <>
+        {TABS}
+        <PodiumChart data={dataWithPlayers} dataKey={`Fecha${searchParamFecha}`} />
       </>
     );
   };
@@ -99,7 +141,7 @@ export function HomeError({ errorMessage, children = undefined }) {
   return (
     <div className="w-100 d-flex align-center flex-col">
       <h1>Error: </h1>
-      <h4>{errorMessage}</h4>
+      <h4 style={{ marginTop: '20px' }}>{errorMessage}</h4>
       {children}
     </div>
   );
